39465e239988881a8171ce367fb1efde3740dd9a
dbgospodinova <diana_gospodinowa@abv.bg>
2017-03-10 17:06:28 +0200

    Initial check-in
    Tests

---
+++ commons-math/src/main/java/gherkin/workshop/commons_math/App.java
@@ -0,0 +1,13 @@
+package gherkin.workshop.commons_math;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
