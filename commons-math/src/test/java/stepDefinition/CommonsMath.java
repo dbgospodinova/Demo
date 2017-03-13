fe3d09726f92490f9114775487a48fd4fcd666ae
dbgospodinova <diana_gospodinowa@abv.bg>
2017-03-13 15:40:05 +0200

    Revert "Implement doubles"
    This reverts commit 35881d3403eced3bc73cbe8ef130232184449a11.

--- commons-math/src/test/java/stepDefinition/CommonsMath.java
+++ commons-math/src/test/java/stepDefinition/CommonsMath.java
@@ -9,13 +9,11 @@ public class CommonsMath {
 	private static int firstNumber=0;
 	private static int secondNumber=0;
 	private static int thirdNumber=0;
-	private static int fourthNumber=0;
 	private static int result;
 	
 	private static double firstDoubleNumber=0;
 	private static double secondDoubleNumber=0;
 	private static double thirdDoubleNumber=0;
-	private static double fourthDoubleNumber=0;
 	private static double resultDouble;
 	
 	@Given("^two integer numbers: '(\\d+)', '(\\d+)'$")
@@ -26,7 +24,7 @@ public class CommonsMath {
 		secondNumber = second;
 	}
 	
-	@Given("^two double numbers: '(\\d+.\\d+)', '(\\d+.\\d+)'$")
+	@Given("^two double numbers: '(\\d+)', '(\\d+)'$")
 	public void two_possitive_double_numbers(double first, double second) throws Throwable {
 		System.out.println(first);
 		System.out.println(second);
@@ -44,7 +42,7 @@ public class CommonsMath {
 	    thirdNumber = third;
 	}
 	
-	@Given("^three double numbers: '(\\d+.\\d+)', '(\\d+.\\d+)', '(\\d+.\\d+)'$")
+	@Given("^three double numbers: '(\\d+)', '(\\d+)', '(\\d+)'$")
 	public void three_possitive_double_numbers(double first, double second, double third) throws Throwable {
 		System.out.println(first);
 		System.out.println(second);
@@ -53,31 +51,6 @@ public class CommonsMath {
 		secondDoubleNumber = second;
 	    thirdDoubleNumber = third;
 	}
-	
-	@Given("^four integer numbers: '(\\d+)', '(\\d+)', '(\\d+)', (\\d+)$")
-	public void four_possitive_integer_numbers(int first, int second, int third, int fourth) throws Throwable {
-		System.out.println(first);
-		System.out.println(second);
-		System.out.println(third);
-		System.out.println(fourth);
-		firstNumber = first;
-		secondNumber = second;
-	    thirdNumber = third;
-	    fourthNumber = fourth;
-	}
-
-	@Given("^four double numbers: '(\\d+.\\d+)', '(\\d+.\\d+)', '(\\d+.\\d+)', (\\d+.\\d+)$")
-	public void four_possitive_double_numbers(double first, double second, double third, double fourth) throws Throwable {
-		System.out.println(first);
-		System.out.println(second);
-		System.out.println(third);
-		System.out.println(fourth);
-		firstDoubleNumber = first;
-		secondDoubleNumber = second;
-	    thirdDoubleNumber = third;
-	    fourthDoubleNumber = fourth;
-	}
-
 
 	@When("^I sum them$")
 	public void i_sum_them() throws Throwable {
@@ -88,17 +61,17 @@ public class CommonsMath {
 	public void the_sum_of_integers(int sum) throws Throwable {
 		int expectedResult = sum;
 		System.out.println(expectedResult);
-		result = firstNumber+secondNumber+thirdNumber+fourthNumber;
+		result = firstNumber + secondNumber+thirdNumber;
 		System.out.println(result);
 		Assert.assertEquals(expectedResult, result);
 	}
 	
-	@Then("^The result of doubles is '(\\d+.\\d+)'$")
+	@Then("^The result of doubles is '(\\d+)'$")
 	public void the_sum_of_doubles(double sum) throws Throwable {
-		double expectedDoubleResult = sum;
-		System.out.println(expectedDoubleResult);
-		resultDouble = firstDoubleNumber+secondDoubleNumber+thirdDoubleNumber+fourthDoubleNumber;
-		System.out.println(resultDouble);
-		Assert.assertEquals(expectedDoubleResult, resultDouble);
+		double expectedResult = sum;
+		System.out.println(expectedResult);
+		result = firstNumber + secondNumber+thirdNumber;
+		System.out.println(result);
+		Assert.assertEquals(expectedResult, resultDouble);
 	}
 }
