c415e6186f93fef506d93f976ca225d2ef8d9310
dbgospodinova <diana_gospodinowa@abv.bg>
2017-03-13 15:02:30 +0200

    New feature
    Doubles

--- commons-math/src/test/java/stepDefinition/CommonsMath.java
+++ commons-math/src/test/java/stepDefinition/CommonsMath.java
@@ -9,19 +9,31 @@ public class CommonsMath {
 	private static int firstNumber=0;
 	private static int secondNumber=0;
 	private static int thirdNumber=0;
-	private static int fourthNumber=0;
 	private static int result;
 	
-	@Given("^two numbers: '(\\d+)', '(\\d+)'$")
-	public void two_positive_numbers_first_number_second_number(int first, int second) throws Throwable {
+	private static double firstDoubleNumber=0;
+	private static double secondDoubleNumber=0;
+	private static double thirdDoubleNumber=0;
+	private static double resultDouble;
+	
+	@Given("^two integer numbers: '(\\d+)', '(\\d+)'$")
+	public void two_possitive_integer_numbers(int first, int second) throws Throwable {
 		System.out.println(first);
 		System.out.println(second);
 		firstNumber = first;
 		secondNumber = second;
 	}
 	
-	@Given("^three numbers: '(\\d+)', '(\\d+)', '(\\d+)'$")
-	public void three_numbers(int first, int second, int third) throws Throwable {
+	@Given("^two double numbers: '(\\d+)', '(\\d+)'$")
+	public void two_possitive_double_numbers(double first, double second) throws Throwable {
+		System.out.println(first);
+		System.out.println(second);
+		firstDoubleNumber = first;
+		secondDoubleNumber = second;
+	}
+	
+	@Given("^three integer numbers: '(\\d+)', '(\\d+)', '(\\d+)'$")
+	public void three_possitive_integer_numbers(int first, int second, int third) throws Throwable {
 		System.out.println(first);
 		System.out.println(second);
 		System.out.println(third);
@@ -30,30 +42,36 @@ public class CommonsMath {
 	    thirdNumber = third;
 	}
 	
-	@Given("^three numbers: '(\\d+)', '(\\d+)', '(\\d+)', (\\d+)$")
-	public void three_numbers(int first, int second, int third, int fourth) throws Throwable {
+	@Given("^three double numbers: '(\\d+)', '(\\d+)', '(\\d+)'$")
+	public void three_possitive_double_numbers(double first, double second, double third) throws Throwable {
 		System.out.println(first);
 		System.out.println(second);
 		System.out.println(third);
-		System.out.println(fourth);
-		firstNumber = first;
-		secondNumber = second;
-	    thirdNumber = third;
-	    fourthNumber = fourth;
+		firstDoubleNumber = first;
+		secondDoubleNumber = second;
+	    thirdDoubleNumber = third;
 	}
-
 
 	@When("^I sum them$")
 	public void i_sum_them() throws Throwable {
 
 	}
 
-	@Then("^The result is '(\\d+)'$")
-	public void their_sum_should_be_sum(int sum) throws Throwable {
+	@Then("^The result of integers is '(\\d+)'$")
+	public void the_sum_of_integers(int sum) throws Throwable {
 		int expectedResult = sum;
 		System.out.println(expectedResult);
-		result = firstNumber + secondNumber+thirdNumber+fourthNumber;
+		result = firstNumber + secondNumber+thirdNumber;
 		System.out.println(result);
 		Assert.assertEquals(expectedResult, result);
+	}
+	
+	@Then("^The result of doubles is '(\\d+)'$")
+	public void the_sum_of_doubles(double sum) throws Throwable {
+		double expectedResult = sum;
+		System.out.println(expectedResult);
+		result = firstNumber + secondNumber+thirdNumber;
+		System.out.println(result);
+		Assert.assertEquals(expectedResult, resultDouble);
 	}
 }
